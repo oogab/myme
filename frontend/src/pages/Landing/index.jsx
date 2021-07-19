5f1f3162661e26aadf3a029985caefee649ee4ff
oogab <38171845+oogab@users.noreply.github.com>
2021-07-19 11:34:04 +0900

    PJT1 sub1 clone

---
+++ frontend/src/pages/Landing/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../layout';
+import LoginForm from '../../components/LoginForm'
+
+
+const Landing = props => {
+
+  return (
+    <Layout>
+      <LoginForm/>
+    </Layout>
+  );
+};
+
+export default Landing;
