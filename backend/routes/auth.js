dd46bf7cd764d5ce9e0bc2524866700965310b1c
oogab <38171845+oogab@users.noreply.github.com>
2021-07-28 21:30:13 +0900

    update: auth.js async

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -41,7 +41,7 @@ router.post('/join', isNotLoggedIn, async (req, res, next) => {
   }
 })
 
-router.post('/login', isNotLoggedIn, (req, res, next) => {
+router.post('/login', isNotLoggedIn, async (req, res, next) => {
   passport.authenticate('local', (authError, user, info) => {
     if (authError) {
       console.error(authError)
