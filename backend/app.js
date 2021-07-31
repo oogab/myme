9873ba3d007b7596c2f5bbaf05f52ebd0965e2b9
oogab <38171845+oogab@users.noreply.github.com>
2021-07-31 20:04:47 +0900

    update: delete auth.js / update: auth router -> user router

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,6 @@ const app = express()
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
 const { sequelize } = require('./models')
 const indexRouter = require('./routes')
-const authRouter = require('./routes/auth')
 const userRouter = require('./routes/user')
 
 app.set('port', process.env.PORT || 3000)
