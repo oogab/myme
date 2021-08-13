d11499c9df3a6e76ce7568ce5b6de767cbc5af49
yuribaek <chsem145@naver.com>
2021-08-13 10:59:11 +0900

    update : weather ui updated

---
+++ frontend/weather/src/components/Clock/styles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+const Wrapper = styled.div`
+    position: absolute;
+    top: 30px;
+    left: 1520px;
+    & > *{
+        display: inline-block;
+        padding:10px;
+    }
+`;
+export default Wrapper;
