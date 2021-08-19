819a47710c37e6e6b7094e7a1c9a22b3ec05938b
yuribaek <chsem145@naver.com>
2021-08-19 14:24:57 +0900

    fix : mirror youtube css changed

--- frontend/Mirror/src/components/Routine/RoutineRootComponent/styles.jsx
+++ frontend/Mirror/src/components/Routine/RoutineRootComponent/styles.jsx
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 const Wrapper = styled.div`
     .progress > div{
-        min-width: 450px;
-        position: absolute;
-        bottom: 47px;
+        width: 450px;
     }
     .progress-expanded > div{
         z-index:50;
-        width: 1400px;
+        width:1400px;
+        position: fixed;
+        left: calc((100% - 1400px)/2);
+        top: 56px;
     }
     .no-routine{
         padding:10px;
